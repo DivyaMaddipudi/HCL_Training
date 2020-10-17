54484ba78ad3ed01ccf30a9587182866b183f2f5
SatyaDivya <divya.maddipudi1@gmail.com>
2020-10-18 01:38:02 +0530

    Added HCL day2 lab assignments

--- lab_assignment_2/src/lab_assignment/q4/Account.java
+++ lab_assignment_2/src/lab_assignment/q4/Account.java
@@ -2,21 +2,38 @@ package lab_assignment.q4;
 
 import java.util.Random;
 
-public class Account {
+class Account{
 	
 	private String memberName;
-	private String accountBalance;
+	private double accountBalance;
 	
 	Random random = new Random();
 	private String accountNumber = "" + random.nextInt(10) + random.nextInt(10) + random.nextInt(10) + random.nextInt(10);
 	
-	public Account(String memberName, String accountBalance) {
-		super();
+	public Account() {
+		
+	}
+	
+	public Account(String memberName, double accountBalance) {
 		this.memberName = memberName;
 		this.accountBalance = accountBalance;
 	}
-	
-	
-	
 
+	public double getAccountBalance() {
+		return accountBalance;
+	}
+
+	public void setAccountBalance(double accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+
+	public void deposit(double amount) {
+		setAccountBalance(getAccountBalance()+amount);
+	}
+	
+	@Override
+	public String toString() {
+		return "Account [memberName=" + memberName + ", accountBalance=" + accountBalance + ", accountNumber="
+				+ accountNumber + "]";
+	}
 }
